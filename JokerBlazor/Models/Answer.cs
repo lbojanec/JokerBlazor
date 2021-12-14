584c99c17cd3fb96a65914378f8dc3508d377a9c
Luka Bojanec <bojanec@gmail.com>
2021-12-14 15:59:05 +0100

    prvi commit

---
+++ JokerBlazor/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokerBlazor.Models
+{
+    public class Answer
+    {
+        public string c_IdAnswer { get; set; }
+        public string c_AnswerText { get; set; }
+        public bool c_IsCorrect { get; set; }
+        public string c_IdQuestion { get; set; }
+    }
+}
---
+++ JokerBlazor/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokerBlazor.Models
+{
+    public class Question
+    {
+        public string c_IdQuestion { get; set; }
+        public string c_QuestionText { get; set; }
+        public bool c_IsCurrent { get; set; }
+        public string c_IdType { get; set; }
+        public string c_IdDifficulty { get; set; }
+        public string c_IdCategory { get; set; }
+        public string c_IdLanguage { get; set; }
+    }
+}
